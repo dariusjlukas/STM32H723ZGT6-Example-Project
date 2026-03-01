f1f8260fff4fbd0d72d02e0b3e2e78962c2e781b
Darius J. Lukas <dariusjlukas@gmail.com>
2026-03-01 17:10:42 -0500

    Update printing logic

--- App/Src/app_main.cpp
+++ App/Src/app_main.cpp
@@ -4,14 +4,20 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 
+const char led_on[] = {"LED On\r\n"};
+const char led_off[] = {"LED Off\r\n"};
+
 extern "C" void app_init(void)
 {
 }
 
 extern "C" void app_loop(void)
 {
-    const char buf[] = {"LED Toggled\r\n"};
-    HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-    CDC_Transmit_HS((uint8_t*)buf, strlen(buf));
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+    CDC_Transmit_HS((uint8_t*)led_on, strlen(led_on));
+    HAL_Delay(1000);
+
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    CDC_Transmit_HS((uint8_t*)led_off, strlen(led_off));
     HAL_Delay(1000);
 }
